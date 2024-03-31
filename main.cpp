4eaafb4b1a79a8d82a15b7ae357781dca60c2bd5
1194713881@qq.com <1194713881@qq.com>
2024-03-31 14:42:27 +0800

    template+future

--- main.cpp
+++ main.cpp
@@ -3,60 +3,35 @@
 #include<thread>
 #include<iostream>
 using namespace std;
-/*
-有些场景 希望获得任务返回值
-如： 1+...+30000的和
-question 1.怎么设计run函数返回值接受任意类型
-question 2.怎么知道任务执行完了？
-*/
-using uLong = unsigned long long;
-class MyTask :public Task
+
+
+int sum1(int a1,int a2)
 {
-public:
-	MyTask(int begin,int end)
-		:begin_(begin)
-		,end_(end)
-	{}
-	// 怎么设计run函数的返回值，可以表示任意的类型
-	// C++17 Any类型
-	AnyType run() override
+	int sum=0;
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+	for(int i=a1;i!=a2;i++)
 	{
-		std::cout << "start " << std::this_thread::get_id() << std::endl;
-		uLong sum = 0;
-		for (int i = begin_; i <= end_; i++)
-			sum += i;
-		std::cout << "end "  << std::this_thread::get_id() << "subsum is " << sum<< std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(3));
-		return sum;
+		sum += a1;
 	}
-private:
-	int end_;
-	int begin_;
-};
+	return sum;
+}
+using uLong = unsigned long long;
 int main()
 {
 	{
 		ThreadPool pool;
 		// 用户设置线程池的工作模式
-		pool.setMode(PoolMode::MODE_CACHED);
+		pool.setMode(PoolMode::MODE_FIXED);
 		// 开始启动线程池
 		pool.start(2);
-		pool.submitTask(std::make_shared<MyTask>(1, 2000000));
-		Result res1 = pool.submitTask(std::make_shared<MyTask>(1, 200000));
-		Result res2 = pool.submitTask(std::make_shared<MyTask>(200001, 400000));
-		Result res3 = pool.submitTask(std::make_shared<MyTask>(400001, 600000));
-		//std::this_thread::sleep_for(std::chrono::seconds(30));
+		future<int> res1= pool.submitTask(sum1,20,30);
+		future<int> res2= pool.submitTask(sum1,20,30);
+		future<int> res3= pool.submitTask(sum1,20,30);
+		future<int> res4= pool.submitTask(sum1,20,30);
+		future<int> res5= pool.submitTask(sum1,20,30);
+		future<int> res6= pool.submitTask(sum1,20,30);
+		
+		cout << res1.get() << endl;
 	}
 
-	//uLong sum1 = res1.get().cast_<uLong>();
-	//uLong sum2 = res2.get().cast_<uLong>();
-	//uLong sum3 = res3.get().cast_<uLong>();
-	//pool.~ThreadPool();
-	//cout << "sum is "<< (sum1 + sum2 + sum3) << endl;
-	//uLong sum = 0;
-	//for (int i = 1; i <= 600000; i++)
-	//	sum += i;
-	//cout << "sum is " << sum << endl;
-	//std::this_thread::sleep_for(std::chrono::seconds(5));
-	//getchar();
 }
--- threadpool.h
+++ threadpool.h
@@ -9,134 +9,20 @@
 #include<condition_variable>
 #include<functional>
 #include<thread>
-// 实际开发中不用using namespace
-// 
+#include <future>
+#include <unordered_map>
 
-class AnyType
+const int TASK_MAX_THRESHHOLD = 3;
+const int THREAD_MAX_THREADHHOLD = 8;
+const int THREAD_PER_WAIT_TIME = 1;
+const int THREAD_MAX_WAIT_COUNT = 2;
+
+// 线程池支持的两种模式
+// enum class 访问枚举型加上作用域
+enum class PoolMode
 {
-public:
-	AnyType() = default;
-	~AnyType() = default;
-
-	//unique_ptr将左值引用的拷贝构造和赋值delete
-	//只允许有一个 初始化or move
-	AnyType(const AnyType&) = delete;
-	AnyType& operator=(const AnyType&) = delete;
-	AnyType(AnyType&&) = default;   // 允许右值引用
-	AnyType& operator=(AnyType&&) = default; // 允许右值引用
-
-	// 让AnyType接收
-	template<typename T>
-	// Base* base_ = new Dereive<T>(data)
-	// 传入的值（任意类型data) --赋值-- 派生类初始化 ---- base_指向派生类
-	//Any(T data):base_(new Derive<T>(data))
-	//{}
-	AnyType(T data)
-	{
-		//base_ = new Derive<T>(data) // 传入的参数存在子类中  用父类指向子类---父类指向任意类型的子类
-		base_ = std::make_unique<Derive<T>>(data);
-	}
-
-	// 把存储的data_数据提取出来
-	template<typename T>
-	T cast_()
-	{
-		// base_找到指向的Derive并取出data
-		// 基类指针转成子类？  一般都是子类转基类  基类指向子类没问题(父类类型少，变成子类），子类指向父类需要注意（子类类型多 变父类？）
-		// RTTI  dynamic_cast<>
-		//std::unique_ptr<Derive<T>> pd = dynamic_cast<std::unique_ptr<Derive<T>>>(base_);
-		
-		Derive<T>* pd = dynamic_cast<Derive<T>*>(base_.get()); // base_.get()智能指针获取裸指针  
-		// 什么时候转失败？ base_本来指向的是int类型的data 而强转为其他的如long
-		if (pd == nullptr)  // 类型不匹配
-		{
-			throw "type is unmatched ";
-		}
-		return pd->data_;
-
-	}
-private:
-	class Base  // 他用的基类身份
-	{
-	public:
-		// 如果基类的派生类在堆上创建，delete基类指针后 派生类无法调用析构
-		virtual ~Base() = default;
-	};
-
-	// 派生类
-	template<typename T>
-	class Derive : public Base
-	{
-	public:
-		Derive(T data):data_(data)
-		{}
-		T data_;
-	};
-private:
-	// 基类指针 指向 派生类
-	std::unique_ptr<Base> base_;  // 基类指针类型的成员变量
-};
-
-// 实现一个信号量类semaphore
-class Semaphore
-{
-public:
-	Semaphore(int limit=0):resLimit_(limit)
-	{}
-	~Semaphore() = default;
-
-	// 获取一个信号量资源
-	void wait()
-	{
-		std::unique_lock<std::mutex> lock(mtx_);
-		cond_.wait(lock, [&]() {return resLimit_ > 0; });
-		resLimit_--;
-	}
-	// 增加一个信号量资源
-	void post()
-	{
-		std::unique_lock<std::mutex> lock(mtx_);
-		resLimit_++;
-		cond_.notify_all();
-	}
-private:
-	int resLimit_;
-	std::mutex mtx_;
-	std::condition_variable cond_;
-};
-
-class Task;
-//返回结果
-//arg...s
-class Result
-{
-public:
-	Result(std::shared_ptr<Task> task, bool isValid = true);
-	~Result() = default;
-
-	// 1. 如何获取任务执行完的返回值
-	void setVal(AnyType any);
-	// 2.get方法，用户调用这个方法获取task返回值
-	AnyType get();
-private:
-	AnyType any_;   // 存储任务的返回值
-	Semaphore sem_; // 信号量
-	std::shared_ptr<Task> task_;  // 智能指针指向task 不要提前析构了
-	std::atomic_bool isValid_;
-};
-
-// 任务抽象基类
-class Task
-{
-public:
-	Task();
-	~Task() = default;
-	void exec();
-	void setResult(Result *res);
-    // 用户可以自定义任意任务类型，从Task继承，重写run方法，实现自定义任务处理
-	virtual AnyType run() = 0;
-private:
-	Result* result_;  // 千万不能用shared_ptr 造成循环引用  强智能指针互相引用问题！
+	MODE_FIXED,  // 固定容量
+	MODE_CACHED, // 可调节容量
 };
 
 // 线程类
@@ -146,59 +32,138 @@ public:
 	// 线程函数对象类型
 	using ThreadFunc = std::function<void(size_t)>;
 	
-	Thread(ThreadFunc func);
-	~Thread();
-	void start();
-	size_t getThreadId();
+	Thread(ThreadFunc func)
+		:func_(func)    // 接受bind的仿函数 保存
+		,threadsKey_(threadsCount_++) // 先赋值 再+1
+	{ }
+	~Thread() = default;
+	void start()
+	{
+		std::thread t(func_,threadsKey_);  // C++来说 线程对象t 和线程函数func_
+		t.detach(); //分离线程 防止出了函数就挂掉 pthread_detach
+	}
+	size_t getThreadId() {return threadsKey_;}
 
 private:
 	ThreadFunc func_;  // 存一个函数对象 传进来一个可调用对象包装器，存起来
-	static size_t threadsCount;
-	size_t threadsKey;
-};
-// 线程池支持的两种模式
-// enum class 访问枚举型加上作用域
-enum class PoolMode
-{
-	MODE_FIXED,  // 固定容量
-	MODE_CACHED, // 可调节容量
+	static size_t threadsCount_;   // 所有对象共享
+	size_t threadsKey_;
 };
 
-/*
-example:
-ThreadPool pool;
-pool.start(4);
-class MyTask:public Task
-{
-public:
-	MyTask() //构造函数初始化参数 
-	{};  
-	void run() override
-	{//线程代码...}
-private:
-	parameter
-}
-pool.submitTask(std::mak_shared<MyTask>());
-*/
+size_t Thread::threadsCount_=0;
+
+
 
 // 线程类型
 class ThreadPool
 {
 public:
 	// 线程池构造
-	ThreadPool();
+	ThreadPool()
+		: initThreadSize_(0)
+	, taskSize_(0)
+	, idelThreadSize_(0)
+	, curTthreadSize_(0)
+	, taskQueMaxThreshHold_(TASK_MAX_THRESHHOLD)
+	, threadSizeThreshHold_(THREAD_MAX_THREADHHOLD)
+	, PoolMode_(PoolMode::MODE_FIXED)
+	, isPoolRunning_(false)
+{}
+
 	// 线程池析构
-	~ThreadPool();
+	~ThreadPool()
+	{
+		isPoolRunning_ = false;
+		notEmpty_.notify_all();
+		std::unique_lock<std::mutex> lock(taskQueMxt_);
+		exitCond_.wait(lock, [&]()->bool {return curTthreadSize_ == 0; });   // 等待所有线程结束
+	}
 	//设置工作模式
-	void setMode(PoolMode mode);
+	void setMode(PoolMode mode)
+	{
+		if (checkRunningState())
+			return;
+		PoolMode_ = mode;
+	}
 	// 设置任务上线阈值
-	void setTaskQueMaxThreshHold(int threadhold);
+	void setTaskQueMaxThreshHold(int threadhold)
+	{
+		taskQueMaxThreshHold_ = threadhold;
+	}
 	
-	void setThreadSizeThreshHold(int threadSize);
-	// 提交任务
-	Result submitTask(std::shared_ptr<Task> sp);
+	void setThreadSizeThreshHold(int threadSize)
+	{
+		if (checkRunningState())  // 正在运行中 不允许设置了
+			return;
+		if (PoolMode_ == PoolMode::MODE_FIXED)  // fixed模式 设置干嘛呢
+			return;
+		threadSizeThreshHold_ = threadSize;
+	}
+	// 提交任务 可变惨模板编程 
+	// decltype推导出函数返回值类型 从而实例化future
+	template<typename Func,typename... Args>
+	auto submitTask(Func&& func, Args&&... args) -> std::future<decltype(func(args...))>
+	{
+		// 打包任务
+		using RType = decltype(func(args...));  // 重命名类型
+		auto task = std::make_shared<std::packaged_task<RType()> >(
+			std::bind(std::forward<Func>(func),std::forward<Args>(args)...)
+		);
+		std::future<RType> result = task->get_future();
+
+		std::unique_lock<std::mutex> lock(taskQueMxt_);
+		while(taskQue_.size() >= (size_t)taskQueMaxThreshHold_)  // 任务队列满 提交超时
+		{
+			if (std::cv_status::timeout ==  notFull_.wait_for(lock, std::chrono::seconds(2)))
+			{
+				// 也可以直接抛出异常
+				task = std::make_shared<std::packaged_task<RType()>>([]()->RType {return RType();} ); // 返回0值
+				(*task)();  //智能指针，解引用访问packaed_task 执行一下任务
+				return task->get_future();  // 返回future类型 future.get()阻塞等待任务执行完成
+			}
+		}
+		taskQue_.emplace( [task](){ (*task)(); } ); // 套一层 捕获task
+		taskSize_++;
+
+		//通知notEmpt 可以拿任务了
+		notEmpty_.notify_all();
+
+		if (PoolMode_ == PoolMode::MODE_CACHED
+			&& taskSize_ > idelThreadSize_
+			&& curTthreadSize_ < threadSizeThreshHold_
+			)
+		{
+			auto ptr = std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this,std::placeholders::_1));
+			auto threadIndex = ptr->getThreadId();
+			threadsMap_.emplace(threadIndex,std::move(ptr));
+			threadsMap_[threadIndex]->start();
+			curTthreadSize_++;
+			idelThreadSize_++;
+		}
+		return result;
+	}
 	// 严阵以待
-	void start(int initThreshSize=std::thread::hardware_concurrency()); // 当前系统CPU核心数量
+	void start(int initThreshSize=std::thread::hardware_concurrency())
+	{
+		isPoolRunning_ = true;
+		// 记录初始线程个数
+		initThreadSize_ = initThreshSize;
+		curTthreadSize_ = initThreshSize;
+
+		// 创建线程对象
+		for (int i = 0; i < initThreadSize_; ++i)
+		{
+			auto ptr = std::make_unique<Thread>( std::bind(&ThreadPool::threadFunc, this,std::placeholders::_1) );
+			auto threadId = ptr->getThreadId();
+			threadsMap_.emplace(threadId,std::move(ptr));
+		}
+		// 启动每个线程
+		for (int i = 0; i < initThreadSize_; ++i)
+		{
+			threadsMap_[i]->start();   // key：value即Thread
+			idelThreadSize_++;  //记录初始的空闲线程
+		}
+	}
 
 	// 不允许拷贝赋值
 	ThreadPool(const ThreadPool&) = delete;
@@ -206,10 +171,86 @@ public:
 
 private:
 	// 定义线程函数
-	void threadFunc(size_t threadId);
+	void threadFunc(size_t threadId)
+	{
+		unsigned int  SleepCount = 0;  // 计数空闲线程睡眠了多少次
+		for (;;)
+		{
+			Task task;
+			{
+				// 先获取锁
+				std::unique_lock<std::mutex> lock(taskQueMxt_);
+				// 等待notEmpty
+				//notEmpty_.wait(lock, [&]() ->bool{return taskQue_.size() > 0; });
+				while(taskQue_.size() == 0)  // 双重判断，避免退出时还在wait造成死锁
+				{
+					if (!isPoolRunning_)
+					{
+						// 结束运行 线程退出
+						//threads_.erase();  // 如何退出？ 不知道这个Thread是哪一个啊 用unordermap
+						threadsMap_.erase(threadId);
+						curTthreadSize_--;
+						idelThreadSize_--;
+						// 通知一下 可以不用等待
+						exitCond_.notify_all();
+						return;
+					}
+
+					// 如果是cacahed模式 空闲线程都在等待任务，需要减少
+					if (PoolMode_ == PoolMode::MODE_CACHED
+						&&curTthreadSize_>initThreadSize_)
+					{
+						if (std::cv_status::timeout == notEmpty_.wait_for(lock, std::chrono::seconds(THREAD_PER_WAIT_TIME)))  // 等待并放锁
+						{
+							SleepCount++;
+							if (SleepCount >= THREAD_MAX_WAIT_COUNT && curTthreadSize_ > initThreadSize_)
+							{
+								// 线程退出啦
+								threadsMap_.erase(threadId);
+								curTthreadSize_--;
+								idelThreadSize_--;
+								if (!isPoolRunning_)
+									exitCond_.notify_all();
+								SleepCount = 0;
+								return;
+							}
+						}
+					}
+					else   // fixed 线程死等不自杀
+					{
+						notEmpty_.wait(lock);  // 结束等待：有任务 or 析构--->就绪态重新抢锁
+					}
+				}
+
+				// 取一个任务出来
+				task = taskQue_.front();
+				taskQue_.pop();
+				taskSize_--;
+
+				// 消费了还是>0 来人还可以消费
+				if (taskQue_.size() > 0)
+				{
+					notEmpty_.notify_all();
+				}
+				notFull_.notify_all();
+			}
+			if (task != nullptr)
+			{
+				SleepCount = 0;
+				idelThreadSize_--;  //空闲线程开始工作
+				task();  //  执行任务 注意任务提交失败不会到达这里，因此任务提交失败之后就执行返回
+				idelThreadSize_++;  //处理完了 空闲
+			}
+		}
+	}
 
 	//检测poo的运行状态
-	bool checkRunningState() const;
+	bool checkRunningState() const
+	{
+		if (isPoolRunning_)
+			return true;
+		else return false;
+	}
 private:
 	std::vector<std::unique_ptr<Thread>> threads_; //线程列表 裸指针需要手动析构 用智能指针 容器析构，元素析构 智能指针删除
 
@@ -218,14 +259,12 @@ private:
 	int initThreadSize_;   //初始的线程数量
 	std::atomic_int idelThreadSize_; //空闲线程数量
 	std::atomic_int curTthreadSize_;     // 记录当前线程数量
-	//不能裸指针 要保留用户传入的 完成后自动释放
-
-	//std::queue<Task*>    //需要发生多态 故用指针，但是用户可能临时任务，拿一个析构的对象？？
-	std::queue<std::shared_ptr<Task>> taskQue_;//任务队列
-	std::atomic_int taskSize_;  //任务的数量 原子类型
-	
-
 	int threadSizeThreshHold_; // 线程数量上线阈值
+
+	// using Task = std::function<返回值()>  返回不知道 加个中间层无返回值的lambda
+	using Task = std::function<void()>;   // 内部封装的task 生命周期确定
+	std::queue<Task>  taskQue_;
+	std::atomic_int taskSize_;  //任务的数量 原子类型
 	int taskQueMaxThreshHold_;    //任务阈值
 	//同时操作任务队列 
 	std::mutex taskQueMxt_;    //保证任务队列的线程安全
